05c64199708af8898f379d665fa435cf8cf81584
Amrita Choudhary <amritachoudhary170@gmail.com>
2026-07-26 10:07:25 +0530

    Add Count no. of occurences in sorted array

--- searching/aggressive_cows.py
+++ searching/aggressive_cows.py
@@ -1,3 +1,17 @@
+# Aggressive Cows
+# Last Updated : 14 Jul, 2026
+# Given an integer array arr[], which denotes the positions of stalls. All the positions are distinct. There are k aggressive cows.
+
+# Assign the cows to the stalls such that the minimum distance between any two cows is maximized.
+
+# Examples: 
+# Input: arr[] = [1, 2, 4, 8, 9], k = 3
+# Output: 3
+# Explanation: The first cow can be placed at arr[0], the second at arr[2], and the third at arr[3]. The minimum distance between any
+# two cows is 3 (between arr[0] and arr[2]), which is the maximum possible among all valid arrangements
+
+
+
 def check(arr, k , distance):
     count = 1
     prev = arr[0]
