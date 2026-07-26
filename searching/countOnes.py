072c2ba893ea0c5c32b638b117d70c7f68e9ecfd
Amrita Choudhar <amritachoudhary170@gmail.com>
2026-07-26 10:30:46 +0530

    Add count 1's in sorted binary array solu

--- searching/countOnes.py
+++ searching/countOnes.py
@@ -1,3 +1,13 @@
+# Input: arr[] = [1, 1, 0, 0, 0, 0, 0]
+# Output: 2
+# Explanation: Count of 1's in the given array is 2.
+
+# Input: arr[] = [1, 1, 1, 1, 1, 1, 1]
+# Output: 7
+
+# Input: arr[] = [0, 0, 0, 0, 0, 0, 0]
+# Output: 0
+
 def countOnes(arr):
     n = len(arr)
     low = 0
